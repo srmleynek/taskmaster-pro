411f5445d9af6e30415a9f7553eec7f19214a2f3
SidMle <srmleynek@gmail.com>
2021-05-27 16:29:09 -0600

    unblur of date fixed?

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,52 +85,47 @@ $(".list-group").on("blur", "textarea", function() {
   $(this).replaceWith(taskP);
 });
 
-// due date was clicked
 $(".list-group").on("click", "span", function() {
-  // get current text
   var date = $(this)
     .text()
     .trim();
 
-  // create new input element
   var dateInput = $("<input>")
     .attr("type", "text")
     .addClass("form-control")
     .val(date);
 
-  // swap out elements
   $(this).replaceWith(dateInput);
 
-  // automatically focus on new element
   dateInput.trigger("focus");
 });
 
-// value of due date was changed
 $(".list-group").on("blur", "input[type='text']", function() {
   // get current text
   var date = $(this)
-    .val()
-    .trim();
+  .val()
+  .trim();
 
   // get the parent ul's id attribute
   var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
 
   // get the task's position in the list of other li elements
   var index = $(this)
-    .closest(".list-group-item")
-    .index();
+  .closest(".list-group-item")
+  .index();
 
-  // update task in array and re-save to localstorage
-  tasks[status][index].date = date;
-  saveTasks();
-
-  // recreate span element with bootstrap classes
+  // update task in array and resave to local storage
+  // tasks[status][index].date = date;
+  // saveTasks();
+  // commenting this out because it won't unblur with it in?
+  
+  // recreate span element
   var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(date);
+  .addClass("badge badge-primary badge-pill")
+  .text(date);
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
